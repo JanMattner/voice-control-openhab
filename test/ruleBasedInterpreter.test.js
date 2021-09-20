813d87bb4c990affcddd5d970bc7ce079c09dfbf
Jan Mattner <Jan.Mattner@aitgmbh.de>
2021-09-20 20:51:26 +0200

    fix item label evaluation

--- test/ruleBasedInterpreter.test.js
+++ test/ruleBasedInterpreter.test.js
@@ -13,6 +13,8 @@ global.Java = {
 };
 
 const rbi = require("../lib/ruleBasedInterpreter");
+jest.mock("../lib/utilities");
+const utilities = require("../lib/utilities").utilities;
 
 const alt = rbi.ruleBasedInterpreter.alt;
 const seq = rbi.ruleBasedInterpreter.seq;
@@ -22,18 +24,15 @@ const itemLabel = rbi.ruleBasedInterpreter.itemLabel;
 const addRule = rbi.ruleBasedInterpreter.addRule;
 const clearRules = rbi.ruleBasedInterpreter.clearRules;
 
-beforeAll(() => {
+beforeEach(() => {
+    clearRules();
+    global.itemRegistry = {
+        getItems: jest.fn().mockReturnValue([])
+    }
+
     global.events = {
         sendCommand: jest.fn()
     };
-
-    global.itemRegistry = {
-        getItems: jest.fn().mockReturnValue([{getLabel: () => "Rollo Esszimmer"}])
-    }
-});
-
-beforeEach(() => {
-    clearRules();
 });
 
 describe("interpretUtterance", () => {
@@ -55,6 +54,151 @@ describe("interpretUtterance", () => {
             var testFunction = jest.fn();
             addRule(testExpression, testFunction);
             rbi.ruleBasedInterpreter.interpretUtterance("fob");
+            expect(testFunction.mock.calls.length).toBe(0);
+        });
+    });
+
+    describe("command expression", () => {
+        it("executes no function when no item expression is contained", () => {
+            var testExpression = cmd("foo", 1);
+            var testFunction = jest.fn();
+            addRule(testExpression, testFunction);
+            rbi.ruleBasedInterpreter.interpretUtterance("foo");
+            expect(testFunction.mock.calls.length).toBe(0);
+            expect(global.events.sendCommand.mock.calls.length).toBe(0);
+        });
+
+        it("sends command to found item", () => {
+            global.itemRegistry = {
+                getItems: jest.fn().mockReturnValue([{getLabel: () => "my item"}])
+            }
+            var cmdParameter = 123;
+            var testExpression = seq([itemLabel(),cmd("foo", cmdParameter)]);
+            var testFunction = jest.fn();
+            addRule(testExpression, testFunction);
+            rbi.ruleBasedInterpreter.interpretUtterance("my item foo");
+            expect(testFunction.mock.calls.length).toBe(0);
+            expect(global.events.sendCommand.mock.calls.length).toBe(1);
+            expect(global.events.sendCommand.mock.calls[0].length).toBe(2);
+            expect(global.events.sendCommand.mock.calls[0][1]).toBe(cmdParameter);
+        });
+    });
+
+    describe("item expression", () => {
+        it("sends command to found item by label", () => {
+            global.itemRegistry = {
+                getItems: jest.fn().mockReturnValue([
+                    {getLabel: () => "item one"},
+                    {getLabel: () => "item two"},
+                    {getLabel: () => "third item"}])
+            }
+            var cmdParameter = 123;
+            var testExpression = seq([itemLabel(),cmd("foo", cmdParameter)]);
+            var testFunction = jest.fn();
+            addRule(testExpression, testFunction);
+            
+            rbi.ruleBasedInterpreter.interpretUtterance("item one foo");
+            expect(testFunction.mock.calls.length).toBe(0);
+            expect(global.events.sendCommand.mock.calls.length).toBe(1);
+            expect(global.events.sendCommand.mock.calls[0].length).toBe(2);
+            expect(global.events.sendCommand.mock.calls[0][1]).toBe(cmdParameter);
+
+            rbi.ruleBasedInterpreter.interpretUtterance("item two foo");
+            expect(testFunction.mock.calls.length).toBe(0);
+            expect(global.events.sendCommand.mock.calls.length).toBe(2);
+            expect(global.events.sendCommand.mock.calls[1].length).toBe(2);
+            expect(global.events.sendCommand.mock.calls[1][1]).toBe(cmdParameter);
+
+            rbi.ruleBasedInterpreter.interpretUtterance("third item foo");
+            expect(testFunction.mock.calls.length).toBe(0);
+            expect(global.events.sendCommand.mock.calls.length).toBe(3);
+            expect(global.events.sendCommand.mock.calls[2].length).toBe(2);
+            expect(global.events.sendCommand.mock.calls[2][1]).toBe(cmdParameter);
+        });
+
+        it("sends command to found item by synonym", () => {
+            var firstName = "item one";
+            var secondName = "item two";
+            var thirdName = "third item";
+            global.itemRegistry = {
+                getItems: jest.fn().mockReturnValue([
+                    {getLabel: () => firstName, getName: () => firstName},
+                    {getLabel: () => secondName, getName: () => secondName},
+                    {getLabel: () => thirdName, getName: () => thirdName}])
+            }
+
+            utilities.getMetadata.mockImplementation((itemName, namespace) => {
+                if (namespace != "synonyms") {
+                    return null;
+                }
+
+                var value = null;
+                switch (itemName) {
+                    case firstName:
+                        value = "bar one, barbar";
+                        break;
+                    case secondName:
+                        value = "second bar";
+                        break;
+                    case thirdName:
+                        value = "bar three";
+                        break;
+                    default:
+                        break;
+                }
+
+                return {value: value};
+            });
+
+            var cmdParameter = 123;
+            var testExpression = seq([itemLabel(),cmd("foo", cmdParameter)]);
+            var testFunction = jest.fn();
+            addRule(testExpression, testFunction);
+            
+            rbi.ruleBasedInterpreter.interpretUtterance("bar one foo");
+            expect(testFunction.mock.calls.length).toBe(0);
+            expect(global.events.sendCommand.mock.calls.length).toBe(1);
+            expect(global.events.sendCommand.mock.calls[0].length).toBe(2);
+            expect(global.events.sendCommand.mock.calls[0][1]).toBe(cmdParameter);
+
+            rbi.ruleBasedInterpreter.interpretUtterance("barbar foo");
+            expect(testFunction.mock.calls.length).toBe(0);
+            expect(global.events.sendCommand.mock.calls.length).toBe(2);
+            expect(global.events.sendCommand.mock.calls[1].length).toBe(2);
+            expect(global.events.sendCommand.mock.calls[1][1]).toBe(cmdParameter);
+
+            rbi.ruleBasedInterpreter.interpretUtterance("second bar foo");
+            expect(testFunction.mock.calls.length).toBe(0);
+            expect(global.events.sendCommand.mock.calls.length).toBe(3);
+            expect(global.events.sendCommand.mock.calls[2].length).toBe(2);
+            expect(global.events.sendCommand.mock.calls[2][1]).toBe(cmdParameter);
+
+            rbi.ruleBasedInterpreter.interpretUtterance("bar three foo");
+            expect(testFunction.mock.calls.length).toBe(0);
+            expect(global.events.sendCommand.mock.calls.length).toBe(4);
+            expect(global.events.sendCommand.mock.calls[3].length).toBe(2);
+            expect(global.events.sendCommand.mock.calls[3][1]).toBe(cmdParameter);
+        });
+    });
+
+    describe("optional expression", () => {
+        it("matches same or none string", () => {
+            var testExpression = seq([opt("bar"), "foo"]);
+            var testFunction = jest.fn();
+            addRule(testExpression, testFunction);
+            rbi.ruleBasedInterpreter.interpretUtterance("foo");
+            expect(testFunction.mock.calls.length).toBe(1);
+            rbi.ruleBasedInterpreter.interpretUtterance("bar foo");
+            expect(testFunction.mock.calls.length).toBe(2);
+        });
+
+        it("does not match other string", () => {
+            var testExpression = seq([opt("bar"), "foo"]);
+            var testFunction = jest.fn();
+            addRule(testExpression, testFunction);
+            rbi.ruleBasedInterpreter.interpretUtterance("other foo");
+            expect(testFunction.mock.calls.length).toBe(0);
+            rbi.ruleBasedInterpreter.interpretUtterance("bar bar foo");
             expect(testFunction.mock.calls.length).toBe(0);
         });
     });
